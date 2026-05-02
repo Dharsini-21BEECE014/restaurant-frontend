7534911f4076eb8c4a5cb9a7dbc121a01780c6df
Arun Kumar M <arun29.7.2003@gmail.com>
2026-05-03 02:09:53 +0530

    api minor change

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://restaurant-backend-iwqv.onrender.com/api",
 });
 
 export default api;
